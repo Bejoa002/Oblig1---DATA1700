c1cccc28330a73e892d9cb87dcd7863844d84a90
Bejoa002 <Benjamin.Balstad.Johnsrud@hotmail.com>
2024-02-15 15:06:44 +0100

    Validation for tomme inputs, og for å sjekke at kun tall kan skrives inn i antall boksen.

--- src/main/resources/static/Kinobiletter.js
+++ src/main/resources/static/Kinobiletter.js
@@ -1,6 +1,21 @@
 const filmer = [];
 
-function kjøp(){
+//Funksjon for å sjekke om det som er skrevet inn i antall er et tall
+function nummer(streng){
+    const tall = Number(streng)
+    let Blank = document.getElementById("blankAntall")
+    let String = "Ikke et tall"
+    String = String.fontcolor("red");
+    if (isNaN(tall)){
+        Blank.innerHTML = String
+        document.getElementById("buyButton").disabled = true    //For å hindre at man kan kjøpe en billett allikevek, deaktiveres kjøp-knappen
+    }
+    else {
+        Blank.innerHTML = ""
+        document.getElementById("buyButton").disabled = false
+    }
+}
+function buy(){
     let empty = 0;
 
     const film = document.getElementById("filmer").value;
@@ -10,47 +25,73 @@ function kjøp(){
     const telefonnr = document.getElementById("telefonnr").value;
     const epost = document.getElementById("epost").value;
 
+    //Validation for filmer
     if(film === "Velg film her"){
         let Blank = document.getElementById("blankFilmer")
-        Blank.removeAttribute("hidden")
-        empty+=1
+        let String = "Du må velge film"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
+        empty += 1
     }
+    else {document.getElementById("blankFilmer").innerHTML = ""}
 
+    //Validation for antall billetter
     if(antall === "0"){
         let Blank = document.getElementById("blankAntall")
-        Blank.removeAttribute("hidden")
-        empty+=1
+        let String = "Du må velge antall billetter"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
+        empty += 1
     }
 
 
     if(fornavn === ""){
         let Blank = document.getElementById("blankFornavn")
-        Blank.removeAttribute("hidden")
+        let String = "Må fylles inn"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
         empty += 1
     }
+    else {document.getElementById("blankFornavn").innerHTML = ""}
+
 
     if(etternavn === ""){
         let Blank = document.getElementById("blankEtternavn")
-        Blank.removeAttribute("hidden")
+        let String = "Må fylles inn"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
         empty += 1
     }
+    else {document.getElementById("blankEtternavn").innerHTML = ""}
+
 
     if(telefonnr === ""){
         let Blank = document.getElementById("blankTelefonnr")
-        Blank.removeAttribute("hidden")
+        let String = "Må fylles inn"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
         empty += 1
     }
+    else {document.getElementById("blankTelefonnr").innerHTML = ""}
+
 
     if(epost === ""){
         let Blank = document.getElementById("blankEpost")
-        Blank.removeAttribute("hidden")
+        let String = "Må fylles inn"
+        String = String.fontcolor("red");
+        Blank.innerHTML = String
         empty += 1
     }
+    else {
+        document.getElementById("blankEpost").innerHTML = ""
+    }
 
+    //Hvis det 1 eller flere input bokser som er blanke, går den ut av funksjonen
     if (empty > 0){
         return
     }
 
+    //Oppretter filmobjektet
     const FilmObjekt = {
         Film: film,
         Antall: antall,
@@ -60,6 +101,8 @@ function kjøp(){
         Epost: epost
     };
     filmer.push(FilmObjekt)
+
+    //Oppretter tabellen
     let ut = "<table><tr>" +
         "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etteranvn</th><th>Telefonnr</th><th>Epost</th>" +
         "</tr>"
@@ -75,6 +118,7 @@ function kjøp(){
 
 }
 
+// funkjson som tømmer hele film-objekt-arrayet og fjerner tabellen
 function slett(){
     filmer.splice(0, filmer.length)
     document.getElementById("billetter").innerHTML = ""
